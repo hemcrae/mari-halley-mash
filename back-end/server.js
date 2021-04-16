35df477c1e6e55dde37340a798b9484e13e5f514
Halley McRae <halleymcrae@MacBook-Air.localdomain>
2021-04-16 14:16:31 -0400

    back-end and front-end communication

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-const mash = require('../back-end/data.json');
+const cors = require('cors');
+const mash = require('./data.json');
 
 app.use(express.json());
+app.use((cors()));
 
 app.get('/', (req, res) => {
     res.send(mash);
