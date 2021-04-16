7f0ba0e3d9967248b73371d6be8bb0aa45e8f3f4
Mari Adamkhan <mariadamkhan@yahoo.ca>
2021-04-16 14:19:26 -0400

    axios calls

--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const mash = require('../back-end/data.json');
+const cors = require('cors');
 
+app.use((cors()));
 app.use(express.json());
 
 app.get('/', (req, res) => {
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,75 +1,91 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+const mashURL = "http://localhost:8080";
+
+export default class App extends React.Component {
 
 
-export default class App extends React.Component{
-    render() {
-    return ( 
-        <>
+  render() {
+    return (
+      <>
         <header className="header">
-            <div className="header-title">
-                <h1>
-                    <span>M.</span>
-                    <span>A.</span>
-                    <span>S.</span>
-                    <span>H.</span>
-                </h1>
-            </div>
-            <div className="header-lists">
+          <div className="header-title">
+            <h1>
+              <span>M.</span>
+              <span>A.</span>
+              <span>S.</span>
+              <span>H.</span>
+            </h1>
+          </div>
+          {/* <div className="header-lists">
                 <ul className="header-list">
                     <li className="header-item"></li>
                 </ul>
-            </div>
+            </div> */}
         </header>
-    
+
         <section className="main">
-            <form className="form">
-                <div className="form__card">
-                    <label className="form__label" for="spouses" name="spouse">
-                        Spouses
-                    </label>
-                    <select className="form__select" id="spouse"name="spouse" multiple>
-                        <option className="form__option">Kim Kardashian</option>
-                    </select>
-                </div>
-                <div className="form__card">
-                    <label className="form__label" for="location" name="location">
-                        Location
-                    </label>
-                    <select className="form__select" id="location" name="location" multiple>
-                        <option className="form__option">Vancouver</option>
-                    </select>
-                </div>
-                <div className="form__card">
-                    <label className="form__label" for="jobs" name="jobs">
-                        Jobs
-                    </label>
-                    <select className="form__select" id="jobs" name="jobs" multiple>
-                        <option className="form__option">Banker</option>
-                    </select>
-                </div>
-                <div className="form__card">
-                    <label className="form__label" for="pet" name="pet">
-                        Pet
-                    </label>
-                    <select className="form__select" id="pet" name="pet" multiple>
-                        <option className="form__option">Dog</option>
-                    </select>
-                </div>
-                <div className="form__card">
-                    <label className="form__label" for="transport" name="transport">
-                        Car
-                    </label>
-                    <select className="form__select" id="transport" name="transport" multiple>
-                        <option className="form__option">Tesla</option>
-                    </select>
-                </div>
-                <button className="form__button">Submit</button>
-            </form>
+          <form className="form">
+            <div className="form__card">
+              <label className="form__label" htmlFor="spouses" name="spouse">
+                Spouses
+              </label>
+              <select
+                className="form__select"
+                id="spouse"
+                name="spouse"
+                multiple
+              >
+                <option className="form__option">Kim Kardashian</option>
+              </select>
+            </div>
+            <div className="form__card">
+              <label className="form__label" htmlFor="location" name="location">
+                Location
+              </label>
+              <select
+                className="form__select"
+                id="location"
+                name="location"
+                multiple
+              >
+                <option className="form__option">Vancouver</option>
+              </select>
+            </div>
+            <div className="form__card">
+              <label className="form__label" htmlFor="jobs" name="jobs">
+                Jobs
+              </label>
+              <select className="form__select" id="jobs" name="jobs" multiple>
+                <option className="form__option">Banker</option>
+              </select>
+            </div>
+            <div className="form__card">
+              <label className="form__label" htmlFor="pet" name="pet">
+                Pet
+              </label>
+              <select className="form__select" id="pet" name="pet" multiple>
+                <option className="form__option">Dog</option>
+              </select>
+            </div>
+            <div className="form__card">
+              <label className="form__label" htmlFor="transport" name="transport">
+                Car
+              </label>
+              <select
+                className="form__select"
+                id="transport"
+                name="transport"
+                multiple
+              >
+                <option className="form__option">Tesla</option>
+              </select>
+            </div>
+            <button className="form__button">Submit</button>
+          </form>
         </section>
-        </>
+      </>
     );
-
-    }
+  }
 }
-
